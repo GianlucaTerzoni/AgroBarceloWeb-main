a1a531febc98253ab9b574408ffe98c14c9cd99d
Eduardo Gomez <eduman.000@gmail.com>
2022-08-30 10:02:20 -0300

    Mapas

--- agro-barcelo-app/src/App.js
+++ agro-barcelo-app/src/App.js
@@ -1,17 +1,18 @@
 import { Login } from "./components/Login";
-import {NavBar} from './components/Navbar'
+import { NavBar } from './components/Navbar'
 
 function App() {
   return (
-    
-    
-     <>
+
+
+    <>
+      
       {/* <NavBar /> 
       <Login /> */}
-     </>
-     
-    
-    
+    </>
+
+
+
   );
 }
 
